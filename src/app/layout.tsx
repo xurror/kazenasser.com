2a88558a03051903fcf1e2273703e9676c757862
xurror <kazenasser@gmail.com>
2024-07-19 07:22:28 +0200

    setup a timeline list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <head>
         <script src="js/paged.polyfill.js" async></script>
       </head>
--- src/components/mdx-layout.tsx
+++ src/components/mdx-layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <main className="@container">
+    <main className="@container" data-theme="dark">
       <article className="prose prose-slate mx-auto mt-8 lg:prose-2xl">
         {children}
       </article>
