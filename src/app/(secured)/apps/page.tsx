552939a5c91d929c78facfd09a7471c2644ec20b
xurror <kazenasser@gmail.com>
2024-08-31 11:42:23 +0200

    setup auth0 authentication

---
+++ src/app/(secured)/apps/page.tsx
@@ -0,0 +1,33 @@
+import { getSession } from '@auth0/nextjs-auth0';
+import Image from 'next/image';
+
+export default async function Page() {
+  const { user, idToken } = await getSession() || {};
+
+	if (user) {
+		console.log(user);
+		fetch('http://localhost:3001/', {
+			headers: {
+				'Authorization': `Bearer ${idToken}`,
+			},
+		}).then((res) => res.json()).then((data) => console.log(data));
+	}
+
+  return (
+		<div>
+			{user && (
+				<div>
+					<Image src={user.picture} alt={user.name} />
+					<h2>{user.name}</h2>
+					<p>{user.email}</p>
+				</div>
+			)}
+			{!user && (
+				<div>
+					<h1>Not logged in</h1>
+					<a href="/api/auth/login">Log in</a>
+				</div>
+			)}
+		</div>
+  );
+}
