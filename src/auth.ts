1ce61d580f1bcf178c763213a53694581e4402c0
xurror <kazenasser@gmail.com>
2024-11-27 16:40:45 +0100

    implement dashboard layout

--- src/auth.ts
+++ src/auth.ts
@@ -2,12 +2,6 @@ import NextAuth from "next-auth";
 import Auth0 from "next-auth/providers/auth0";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Auth0({
-      profile(profile) {
-        console.log("profile:", profile);
-        return { role: profile.role ?? "user", ...profile };
-      },
-    }),
-  ],
+  // debug: true,
+  providers: [Auth0],
 });
